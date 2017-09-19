4c3afc2f6f80af20bf19b215bdb65856cee5bf57
JasonSekhon <jason_2s@hotmail.com>
2017-09-19 11:32:18 -0700

    initial

---
+++ HappyMVC/Controllers/FilesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace HappyMVC.Controllers
+{
+    public class FilesController : Controller
+    {
+        // GET: Files
+        public ActionResult Index()
+        {
+            string[] files = Directory.GetFiles(Server.MapPath("~/TextFiles"));
+            ViewBag.files = files;
+            return View();
+        }
+
+        public ActionResult Contents(int id) {
+            var file = Directory.GetFiles(Server.MapPath("~/TextFiles"))[id];
+            string fileContent = System.IO.File.ReadAllText(file);
+            ViewBag.fileContent = fileContent;
+            return View();
+        }
+    }
+}
---
+++ HappyMVC/HappyMVC/Controllers/ProcessController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HappyMVC.Controllers
+{
+    public class ProcessController : Controller
+    {
+        // GET: Process
+        public ActionResult Index()
+        {
+			var processes = System.Diagnostics.Process.GetProcesses();
+			ViewBag.CurrentProcesses = processes;
+            return View();
+        }
+
+		// GET: Process
+		public ActionResult Concrete()
+		{
+			var processes = System.Diagnostics.Process.GetProcesses();
+			return View(processes);
+		}
+
+		public ActionResult Details(int id)
+		{
+			var process = System.Diagnostics.Process.GetProcessById(id);
+			return View(process);
+		}
+
+	}
+}
